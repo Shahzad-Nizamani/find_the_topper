ea556f94c7820f6d45b05fe92ec477c7cc9d0103
Shahzad Nizamani <nizamanias124@gmail.com>
2025-08-08 15:13:01 +0500

    Fix build script for Render deployment

--- build.sh
+++ build.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Update package list
+apt-get update
+
+# Install dependencies for Chrome
+apt-get install -y wget unzip xvfb libxi6 libgconf-2-4 libnss3 libasound2 fonts-liberation libappindicator3-1 xdg-utils libu2f-udev
+
 # Install Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install -y ./google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome-stable_current_amd64.deb || apt-get -fy install
 
-# Install matching ChromeDriver
-CHROME_VERSION=$(google-chrome --version | grep -oP '\d+\.\d+\.\d+' | head -1)
-wget https://chromedriver.storage.googleapis.com/$CHROME_VERSION/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
-chmod +x chromedriver
-mv chromedriver /usr/local/bin/chromedriver
+# Get the full Chrome version
+CHROME_VERSION=$(google-chrome --version | grep -oP '\d+\.\d+\.\d+\.\d+')
+
+# Download matching ChromeDriver (Chrome for Testing format)
+wget https://storage.googleapis.com/chrome-for-testing-public/${CHROME_VERSION}/linux64/chromedriver-linux64.zip
+
+# Extract and install chromedriver
+unzip chromedriver-linux64.zip
+mv chromedriver-linux64/chromedriver /usr/local/bin/chromedriver
+chmod +x /usr/local/bin/chromedriver
